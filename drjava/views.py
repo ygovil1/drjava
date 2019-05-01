b7671859456dc4205d401d5dbadee0174531f603
Yashodhar Govil <ygovil@princeton.edu>
2019-05-01 14:37:59 -0400

    link up urls and views

--- drjava/views.py
+++ drjava/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "drjava/index.html", {})
+
+
+def ordered(request):
+    return render(request, "drjava/ordered.html", {})
+
